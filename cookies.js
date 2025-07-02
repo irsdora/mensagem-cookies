3b1d006b3a1e299382007c3b90cecbb3441eb3f6
Isadora Silva <silvairs9292@gmail.com>
2025-07-01 23:06:49 -0300

    editando o arquivo javascript

--- cookies.js
+++ cookies.js
@@ -5,3 +5,9 @@ function aceito(){
     localStorage.lggppd = "SIM"
     msgCookies.classList.remove('VISUALIZAR')
 }
+
+if(localStorage.lggppd == 'SIM'){
+   msgCookies.classList.remove('VISUALIZAR')
+}else{
+    msgCookies.classList.add('VISUALIZAR')
+}
